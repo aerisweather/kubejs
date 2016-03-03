ce64e2ec45324362116320ae2d105a98d7f190d7
Edan Schwartz <eschwartz@aerisweather.net>
2016-03-03 16:09:55 -0600

    some useful scripts

---
+++ scripts/make-schedulable.js
@@ -0,0 +1,16 @@
+const co = require('co');
+const Node = require('../lib/Kubernetes/Node');
+
+
+const nodeName = process.argv[2];
+if (!nodeName) { throw new Error('Missing node name arguments'); }
+
+const node = new Node(nodeName);
+
+node.makeSchedulable()
+	.then(() => {
+		process.exit(0);
+	}, (err) => {
+		console.error(err.name, err.stack);
+		process.exit(1);
+	});
---
+++ scripts/redistribute-pods.js
@@ -0,0 +1,37 @@
+const ReplicationController = require('../lib/Kubernetes/ReplicationController');
+const co = require('co');
+const Cli = require('admiral-cli');
+
+const cli = new Cli()
+	.option({
+		name: 'namespace',
+		description: 'k8s namespace in which to redistribute pods',
+		shortFlag: '-n',
+		longFlag: '--namespace',
+		required: true,
+		length: 1
+	})
+
+cli.parse();
+
+if (!cli.params.namespace) {
+	throw new Error('missing namespace option');
+}
+
+function main() {
+	return co(function* () {
+		const rcs = yield ReplicationController.getAll(cli.params.namespace);
+
+		// Scale up all nodes
+		yield rcs.map(rc => rc.reschedule());
+	});
+}
+
+
+main()
+	.then(() => {
+		process.exit(0);
+	}, (err) => {
+		console.error(err.name, err.stack);
+		process.exit(1);
+	});
