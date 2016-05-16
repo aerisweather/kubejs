407786bbadfc5445149004642129a3aa2d33bf65
Seth Miller <smiller@aerisweather.com>
2016-05-16 17:27:07 -0500

    Added namespace option when getting stats summary for a namespace.

--- bin/commands/stats/get-all-cadvisor-containers.js
+++ bin/commands/stats/get-all-cadvisor-containers.js
@@ -6,59 +6,62 @@ const _ = require('lodash');
 
 const cluster = new Cluster();
 
-return co(function* () {
-	const kubeNodes = yield cluster.getAllNodes();
+module.exports = function(namespace) {
 
-	const extNodeIps = yield kubeNodes.map((node) => node.getExternalIp());
-	const cAdvisorUrls = extNodeIps.map((externalIp) =>`http://${externalIp}:4194`);
-	const cACluster = new CACluster(cAdvisorUrls);
+	return co(function*() {
+		const kubeNodes = yield cluster.getAllNodes();
 
-	// Get containers for our namespace
-	const containersPerNode = yield kubeNodes.map((node) => {
-		return node.getAllPods('amp-staging');
-	});
+		const extNodeIps = yield kubeNodes.map((node) => node.getExternalIp());
+		const cAdvisorUrls = extNodeIps.map((externalIp) =>`http://${externalIp}:4194`);
+		const cACluster = new CACluster(cAdvisorUrls);
 
-	// Alphabetical list of pods
-	const pods = _.flatten(containersPerNode).sort((a, b) => {
-		if (a.name < b.name) {
-			return -1;
-		}
-		return 1;
-	});
+		// Get containers for our namespace
+		const containersPerNode = yield kubeNodes.map((node) => {
+			return node.getAllPods(namespace);
+		});
 
-	const podStats = yield pods.map((pod) => {
-		return pod.getContainerIds()
-			.then((containerIds) => {
-				return Promise.all(containerIds.map((containerId) => cACluster.getContainerStats(containerId)));
+		// Alphabetical list of pods
+		const pods = _.flatten(containersPerNode).sort((a, b) => {
+			if (a.name < b.name) {
+				return -1;
+			}
+			return 1;
+		});
+
+		const podStats = yield pods.map((pod) => {
+			return pod.getContainerIds()
+				.then((containerIds) => {
+					return Promise.all(containerIds.map((containerId) => cACluster.getContainerStats(containerId)));
+				})
+				.then((containerStats) => {
+					pod._stats = containerStats;
+					return pod;
+				})
+		});
+
+		podStats.map((pod) => {
+			console.log(`${pod.name} (${pod.namespace})`);
+			pod._stats.map((stats) => {
+				const condensedStats = condenseStats(stats);
+				console.log(`\t CPU: ${condensedStats.cpu.current}\tMemHot: ${formatMem(condensedStats.memory.hot)}\t MemTotal: ${formatMem(condensedStats.memory.total)}\n`);
 			})
-			.then((containerStats) => {
-				pod._stats = containerStats;
-				return pod;
-			})
-	});
+		});
+	})
+		.catch(err => console.error(err.stack));
 
-	podStats.map((pod) => {
-		console.log(`${pod.name} (${pod.namespace})`);
-		pod._stats.map((stats) => {
-			const condensedStats = condenseStats(stats);
-			console.log(`\t CPU: ${condensedStats.cpu.current}\tMemHot: ${formatMem(condensedStats.memory.hot)}\t MemTotal: ${formatMem(condensedStats.memory.total)}\n`);
-		})
-	});
-})
-	.catch(err => console.error(err.stack));
-
-function condenseStats(cAdvisorContainerStats) {
-	const latestStats = _.last(cAdvisorContainerStats.stats);
-	return {
-		cpu:    {
-			current: latestStats.cpu.load_average
-		},
-		memory: {
-			total: latestStats.memory.usage,
-			hot:   latestStats.memory.working_set
+	function condenseStats(cAdvisorContainerStats) {
+		const latestStats = _.last(cAdvisorContainerStats.stats);
+		return {
+			cpu:    {
+				current: latestStats.cpu.load_average
+			},
+			memory: {
+				total: latestStats.memory.usage,
+				hot:   latestStats.memory.working_set
+			}
 		}
 	}
-}
+};
 
 function formatMem(memInBytes) {
 	return padRight(""+Math.round(memInBytes / 1024 / 10.24) / 100, 7, '0') + 'M';
